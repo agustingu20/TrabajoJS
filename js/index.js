2c5e2113d41d461ab3c893e9a4b3351b7cdfdfc6
Fabio <fabioramosnic@gmail.com>
2021-04-05 16:13:11 -0300

    Comienzo de indexjs y vinculado de js con html

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const cards = document.getElementById("divCards")
+const productoEnviado = document.getElementById("enviarProducto")
+const contenidoCards = document.getElementById("contenidoCard")
+const imagenCards = document.getElementById("imagenCard")
+const PrecioInput = document.getElementById("precio");
+const NombreInput = document.getElementById("nombre");
+const DescripInput = document.getElementById("descripcion");
