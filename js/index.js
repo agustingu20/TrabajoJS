efdda38a69ded414b99acd9ff02ff246c2ab1f56
Fabio <fabioramosnic@gmail.com>
2021-04-07 03:32:52 -0300

    Funcion de agregar cards al index

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
-const cards = document.getElementById("divCards")
-const productoEnviado = document.getElementById("enviarProducto")
-const contenidoCards = document.getElementById("contenidoCard")
-const imagenCards = document.getElementById("imagenCard")
-const PrecioInput = document.getElementById("precio");
-const NombreInput = document.getElementById("nombre");
-const DescripInput = document.getElementById("descripcion");
+const cardsDiv = document.getElementById('divCards')
+const cardimagen = document.getElementById('imagenCard')
+const cardtitulo = document.getElementById('tituloCard')
+const carddesc = document.getElementById('descCard')
+const cardprecio = document.getElementById('precioCard')
+const json = localStorage.getItem("productos"); // Traer de localStorage el dato asociado a la key del producto.
+const data = JSON.parse(json); // Convertir datos de un string JSON a código JavaScript.
+let productos = data || [];
+let productoId = "";
+// const productoEnviado = document.getElementById("enviarProducto")
+
+function mostrarCards() {
+    const cardsMap = productos.map(function (producto) {
+        return ` 
+        <div class="card1">
+                <img id="imagenCard" src="${producto.imagen}" alt="Avatar">
+            <div id="contenidoCard" class="containercard">
+                <h4 id="tituloCard"><b>${producto.nombre}</b></h4>
+                <h5 id="descCard">${producto.descripcion}</h5>
+                <p id="precioCard"><b>${producto.cantidad}</b></p>
+                <p><button class="btn2">Comprar</button></p>
+            </div>
+        </div>
+    `
+    })
+    cardsDiv.innerHTML = cardsMap.join("");
+}
+
+mostrarCards();
+
+
+
+
+
