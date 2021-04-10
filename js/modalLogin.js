9496efbf2acf6dff2316570419fe5cb47b3b0d29
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-04-10 11:04:27 -0300

    correccion alertas al login de usuarios

--- js/modalLogin.js
+++ js/modalLogin.js
@@ -9,23 +9,29 @@ const adminLogin = { email: 'admin@admin.com', pass: 'Admin123' };
 formularioIngreso.onsubmit = function (e) {
     e.preventDefault();
     const usuariosLocal = JSON.parse(localStorage.getItem('usuarios')) || [];
+    const coincideEmailAdmin = adminLogin.email === emailInputModal.value;
+    const coincidePassAdmin = adminLogin.pass === contraseñaInputModal.value;
+
+    if (coincideEmailAdmin && coincidePassAdmin) {
+        window.location.href = './admin.html';
+        alertaDiv.classList.add('d-none');
+        alertaDivBloqueo.classList.add('d-none');
+    }
     for (let i = 0; i < usuariosLocal.length; i++) {
         const usuario = usuariosLocal[i];
         const coincideEmailUsuario = emailInputModal.value === usuario.email;
         const coincidePassUsuario = contraseñaInputModal.value === usuario.pass;
-        const coincideEmailAdmin = adminLogin.email === emailInputModal.value;
-        const coincidePassAdmin = adminLogin.pass === contraseñaInputModal.value;
 
         if (coincideEmailUsuario && coincidePassUsuario && usuario.habilitacion === 'Habilitado') {
             window.location.href = './index.html';
             alertaDiv.classList.add('d-none');
             alertaDivBloqueo.classList.add('d-none');
-        } else if (coincideEmailAdmin && coincidePassAdmin) {
-            window.location.href = './admin.html';
         } else if (coincideEmailUsuario && coincidePassUsuario && usuario.habilitacion === 'Deshabilitado') {
             alertaDivBloqueo.classList.remove('d-none');
+            alertaDiv.classList.add('d-none');
         } else {
             alertaDiv.classList.remove('d-none');
+            alertaDivBloqueo.classList.add('d-none');
         }
     }
     formularioIngreso.reset();
