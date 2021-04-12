3de951bb7415e0c00ca0601c20d87dd34d6941f7
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-04-11 22:58:42 -0300

    logueo de usuario

--- js/modalLogin.js
+++ js/modalLogin.js
@@ -3,8 +3,68 @@ const alertaDiv = document.getElementById('alerta');
 const emailInputModal = document.getElementById('inputEmailModal');
 const contraseñaInputModal = document.getElementById('inputContraseñaModal');
 const alertaDivBloqueo = document.getElementById('alertaBloqueo');
+// const registroNavbar = document.getElementById('navbarRegistrado');
+// const sinIngreso = document.getElementById('usuarioIngreso');
+
+const json2 = localStorage.getItem('usuarioRegistrado');
+const data2 = JSON.parse(json2);
+let usuarioRegistrado = data2 || [];
 
 const adminLogin = { email: 'admin@admin.com', pass: 'Admin123' };
+
+if (usuarioRegistrado.length !== 0) {
+    function mostrarUsuarioRegistrado() {
+        const registroNavbar = document.getElementById('navbarRegistrado');
+        const sinIngreso = document.getElementById('usuarioIngreso');
+        const usuarioReg = usuarioRegistrado.map(function (usuarioRegistrado) {
+            return `
+        <div class="container-fluid">
+            <div class="logo-div">
+                <img class="logonav" src="./img/LOGO3" alt="logO" />
+            </div>
+            <section class="pbusqueda">
+                <div class="busqueda">
+                    <input type="text" placeholder="Buqueda" required />
+                    <div class="btn1">
+                        <i class="fas fa-search-plus icon"></i>
+                    </div>
+                </div>
+            </section>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse navbar1" id="navbarNavDropdown">
+                <ul class="navlis">
+                    <li><a href="./index.html#indexNav">Inicio</a></li>
+                    <li><a href="./index.html#divCards">Categorias</a></li>
+                    <li><a href="./index.html#about">About</a></li>
+                    <li class="px-0">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle navbar1" type="button"
+                                id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                ${usuarioRegistrado.nombre}
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a class="dropdown-item" href="#">Ver Perfil</a></li>
+                                <li><a class="dropdown-item" href="#">Cerrar Sesión</a></li>
+                                </ul>
+                                </div>
+                                </li>
+                                <a class="btn-cart-a" href="" data-bs-toggle="modal" data-bs-target="#modalCarrito"><i
+                                class="fas fa-shopping-cart btn-cart"></i></a>
+                                </ul>
+                                </div>
+                                </div>
+                                `;
+        });
+        registroNavbar.classList.remove('nav-registrado');
+        sinIngreso.classList.add('d-none');
+        registroNavbar.innerHTML = usuarioReg;
+
+    };
+    mostrarUsuarioRegistrado();
+}
 
 formularioIngreso.onsubmit = function (e) {
     e.preventDefault();
@@ -17,6 +77,7 @@ formularioIngreso.onsubmit = function (e) {
         alertaDiv.classList.add('d-none');
         alertaDivBloqueo.classList.add('d-none');
     }
+
     for (let i = 0; i < usuariosLocal.length; i++) {
         const usuario = usuariosLocal[i];
         const coincideEmailUsuario = emailInputModal.value === usuario.email;
@@ -26,6 +87,12 @@ formularioIngreso.onsubmit = function (e) {
             window.location.href = './index.html';
             alertaDiv.classList.add('d-none');
             alertaDivBloqueo.classList.add('d-none');
+
+
+            usuarioRegistrado.push(usuario);
+            const json2 = JSON.stringify(usuarioRegistrado);
+            localStorage.setItem('usuarioRegistrado', json2);
+
         } else if (coincideEmailUsuario && coincidePassUsuario && usuario.habilitacion === 'Deshabilitado') {
             alertaDivBloqueo.classList.remove('d-none');
             alertaDiv.classList.add('d-none');
@@ -36,3 +103,4 @@ formularioIngreso.onsubmit = function (e) {
     }
     formularioIngreso.reset();
 }
+// mostrarUsuarioRegistrado();
